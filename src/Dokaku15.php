16f32d98cec3739cbf1572b17a6ce284a34a7cd6
Akihito Koriyama <akihito.koriyama@gmail.com>
2019-04-17 23:15:24 +0900

    Update Dokaku15.php

--- src/Dokaku15.php
+++ src/Dokaku15.php
@@ -8,7 +8,29 @@ class Dokaku15
 {
     public function run(string $input): string
     {
-        // implement me
-        return '3445';
+        // input
+        $bin =  sprintf( "%08d", decbin((int) $input));
+        $inputs = str_split($bin, 1);
+        // remove no line
+        for ($i = 0; $i < 8; $i++) {
+            if ($inputs[$i] === '0') {
+                unset($inputs[$i]);
+            }
+        }
+        // init
+        $lines = array_keys($inputs);
+        $lines[] = 8 + $lines[0]; // virtual last line
+        $length = count($lines) - 1;
+        $result = [];
+        // count
+        for ($i = 0; $i < $length; $i++) {
+            $distance = $lines[$i + 1] - $lines[$i];
+            $polygon = $distance == 4 ? 5 : $distance + 2;
+            $result[] = $polygon;
+        }
+        // format
+        sort($result);
+
+        return implode('', $result);
     }
 }
